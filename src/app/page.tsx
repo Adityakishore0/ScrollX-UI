44c874c71f8f50b96aca4a78766033b714d07405
Ahdeetai <adityakishore0074@gmail.com>
2025-03-12 20:30:51 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import Home from '@/app/components/home';
 import Navbar from '@/app/components/navbar';
 import Testimonial from '@/app/components/testimonials';
 import { ThemeProvider } from '@/app/context/ThemeContext';
+import TSkeyboard from '@/app/TScontributions/TSkeyboard';
 
 export default function HomePage() {
   return (
