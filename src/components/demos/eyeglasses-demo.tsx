99a769cc950bc57f0513ba0c7cd3548b92aad104
Adityakishore0 <adityakishore0074@gmail.com>
2025-04-27 14:20:03 +0530

    feat: <eyeglasses /> added

---
+++ src/components/demos/eyeglasses-demo.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React from "react";
+import EyeglassesFrame from "@/components/ui/eyeglasses";
+
+export default function EyeglassesDemo() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen  p-8">
+      <EyeglassesFrame
+        colorOptions={{
+          black: "bg-black border-black",
+          brown: "bg-amber-800 border-amber-800",
+          silver: "bg-gray-400 border-gray-400",
+          green: "bg-green-700 border-green-700",
+          pink: "bg-pink-500 border-pink-500",
+        }}
+        initialColor="silver"
+        className="rounded-lg"
+        showColorPicker={true}
+        size="md"
+      />
+    </div>
+  );
+}
