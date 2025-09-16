8919917b6b72a66189eece4932abb61717a8b63f
Ahdeetai <adityakishore0074@gmail.com>
2025-09-16 20:06:05 +0530

    Update navItems.ts

--- src/constants/navItems.ts
+++ src/constants/navItems.ts
@@ -104,7 +104,7 @@ const navigation: NavItem[] = [
         href: "/docs/components/flowing-logos",
         category: "new",
       },
-      { title: "Folder Tree", href: "/docs/components/folder-tree" },
+      { title: "Folder Tree", href: "/docs/components/folder-tree", category: "new", },
       {
         title: "GlowingBorderCard",
         href: "/docs/components/glowingbordercard",
