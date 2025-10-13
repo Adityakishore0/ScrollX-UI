fe063970ea726b6d27c7eb27b3625578a3eead9a
Adityakishore0 <adityakishore0074@gmail.com>
2025-10-13 23:39:14 +0530

    fix: adjust text colour for GlassHero demo in light mode

--- src/components/demos/glasshero-demo.tsx
+++ src/components/demos/glasshero-demo.tsx
@@ -54,7 +54,7 @@ export default function GlassHeroDemo() {
           </div>
         </Glass>
 
-        <h1 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+        <h1 className="bg-clip-text text-transparent text-center bg-gradient-to-b  from-neutral-600 to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
           Summer is over
           <br />
           Winter has arrived
