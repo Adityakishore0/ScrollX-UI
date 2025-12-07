7bd90347b6f84f3cf1913aa7cc10b8279f37e274
Adityakishore0 <adityakishore0074@gmail.com>
2025-12-07 23:16:09 +0530

    feat: add custom layered-button demo

---
+++ src/components/demos/layeredbuttoncustom-demo.tsx
@@ -0,0 +1,9 @@
+import { LayeredButton } from "@/components/ui/layered-button";
+
+export default function LayeredButtonDemo() {
+  return (
+    <LayeredButton className="bg-blue-600 rounded-full">
+      Custom Blue Pill
+    </LayeredButton>
+  );
+}
