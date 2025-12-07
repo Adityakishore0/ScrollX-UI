5ad236f60b57d1012c1dac11202b866be9ccccb6
Adityakishore0 <adityakishore0074@gmail.com>
2025-12-07 23:16:27 +0530

    feat: add outline variant demo for layered-button

---
+++ src/components/demos/layeredbuttonoutline-demo.tsx
@@ -0,0 +1,5 @@
+import { LayeredButton } from "@/components/ui/layered-button";
+
+export default function LayeredButtonOutlineDemo() {
+  return <LayeredButton variant="outline">Outline</LayeredButton>;
+}
