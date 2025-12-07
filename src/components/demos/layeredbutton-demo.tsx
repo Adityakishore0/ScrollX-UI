318dca97bb9be08e6d9111c5616254624eb2a00d
Adityakishore0 <adityakishore0074@gmail.com>
2025-12-07 23:15:51 +0530

    feat: add default demo for layered-button

---
+++ src/components/demos/layeredbutton-demo.tsx
@@ -0,0 +1,5 @@
+import { LayeredButton } from "@/components/ui/layered-button";
+
+export default function LayeredButtonDemo() {
+  return <LayeredButton variant="default">Default</LayeredButton>;
+}
