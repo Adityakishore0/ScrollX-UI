e5a52ff0b9064cce995ebbe430b9623a2fb187dc
Adityakishore0 <adityakishore0074@gmail.com>
2026-01-24 23:31:22 +0530

    feat: add compact demo for stripe-button

---
+++ src/components/demos/stripebuttoncompact-demo.tsx
@@ -0,0 +1,9 @@
+import { StripeButton } from "@/components/ui/stripe-button";
+
+export default function StripeButtonDemo() {
+  return (
+    <StripeButton className="italic" variant="compact">
+      Enter the Arena!
+    </StripeButton>
+  );
+}
