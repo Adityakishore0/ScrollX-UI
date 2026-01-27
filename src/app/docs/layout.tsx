65a8a690c54f0e9f59b72f9b483d55ee0896573b
Adityakishore0 <adityakishore0074@gmail.com>
2026-01-27 18:48:25 +0530

    refactor: remove borders from sidebar and on-this-page

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -11,7 +11,7 @@ export default function DocsLayout({ children }: { children: ReactNode }) {
       <Navbar />
       <div className="flex-1">
         <div className="lg:grid lg:grid-cols-[minmax(16rem,16rem)_1fr] xl:grid-cols-[minmax(16rem,16rem)_1fr_minmax(16rem,16rem)]">
-          <aside className="hidden lg:block w-full sticky z-[30] top-16 h-[calc(100vh-4rem)] overflow-y-auto border-r border-gray-200 dark:border-gray-800">
+          <aside className="hidden lg:block w-full sticky z-[30] top-16 h-[calc(100vh-4rem)] overflow-y-auto">
             <Sidebar />
           </aside>
           <main className="min-w-0 py-8 px-4 lg:px-8">
@@ -21,7 +21,7 @@ export default function DocsLayout({ children }: { children: ReactNode }) {
               <DocsFooter />
             </div>
           </main>
-          <aside className="hidden xl:block w-full sticky top-16 h-[calc(100vh-4rem)] overflow-y-auto border-l border-gray-200 dark:border-gray-800">
+          <aside className="hidden xl:block w-full sticky top-16 h-[calc(100vh-4rem)] overflow-y-auto">
             <div className="px-6 py-8">
               <OnThisPage />
             </div>
