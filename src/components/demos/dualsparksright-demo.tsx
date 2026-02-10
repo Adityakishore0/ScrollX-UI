7cec807345e9775fde682421f29861e876a93e9d
Adityakishore0 <adityakishore0074@gmail.com>
2026-02-11 00:14:06 +0530

    refactor: update dualsparksright-demo.tsx

--- src/components/demos/dualsparksright-demo.tsx
+++ src/components/demos/dualsparksright-demo.tsx
@@ -1,21 +1,21 @@
-"use client";
+'use client';
 
-import { DualSparks } from "@/components/ui/dualsparks";
+import { DualSparks } from '@/components/ui/dualsparks';
 
 export default function DualSparksRightDemo() {
   return (
     <DualSparks
-      sparkColor="#000000"
-      sparkColorDark="#ffffff"
+      sparkColor='#000000'
+      sparkColorDark='#ffffff'
       sparkSize={2}
       sparkCount={22}
       ringsPerWave={3}
       waveInterval={1200}
       maxRadius={1000}
-      corners="right"
-      className="w-full h-[350px] flex items-center bg-black justify-center"
+      corners='right'
+      className='w-full h-87.5 flex items-center bg-black justify-center'
     >
-      <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans font-bold tracking-tight relative z-20">
+      <h2 className='bg-clip-text text-transparent text-center bg-linear-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans font-bold tracking-tight relative z-20'>
         DualSparks, <br /> Background.
       </h2>
     </DualSparks>
