60ac85e2a3fa7957c7927543b0033f28ff0f8ce1
Adityakishore0 <adityakishore0074@gmail.com>
2026-02-11 00:19:13 +0530

    refactor: update particles-demo.tsx

--- src/components/demos/particles-demo.tsx
+++ src/components/demos/particles-demo.tsx
@@ -1,14 +1,14 @@
-import { Particles } from "@/components/ui/particles";
+import { Particles } from '@/components/ui/particles';
 
 export default function ParticlesDemo() {
   return (
-    <div className="relative w-full h-[400px]">
+    <div className='relative w-full h-100'>
       <Particles
-        color="#fffff"
+        color='#fffff'
         particleCount={25000}
         particleSize={5}
         animate={false}
-        className="z-0 bg-black"
+        className='z-0 bg-black'
       />
     </div>
   );
