6d46a703c09acca1775ea202a650520769e33323
Adityakishore0 <adityakishore0074@gmail.com>
2026-02-11 00:13:37 +0530

    refactor: update dotwave-demo.tsx

--- src/components/demos/dotwave-demo.tsx
+++ src/components/demos/dotwave-demo.tsx
@@ -1,6 +1,6 @@
-"use client";
+'use client';
 
-import { DotWave } from "@/components/ui/dot-wave";
+import { DotWave } from '@/components/ui/dot-wave';
 
 export default function DotWaveDemo() {
   return (
@@ -10,9 +10,9 @@ export default function DotWaveDemo() {
       expansionSpeed={250}
       lightIntensity={0.4}
       fadeIntensity={0.08}
-      className="w-full h-[350px] flex items-center justify-center"
+      className='w-full h-87.5 flex items-center justify-center'
     >
-      <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-600 to-white text-2xl md:text-4xl lg:text-7xl font-sans font-bold tracking-tight relative z-20">
+      <h2 className='bg-clip-text text-transparent text-center bg-linear-to-b from-neutral-600 to-white text-2xl md:text-4xl lg:text-7xl font-sans font-bold tracking-tight relative z-20'>
         Dot Wave, <br /> Background.
       </h2>
     </DotWave>
