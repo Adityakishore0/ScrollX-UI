06b24a263e8bb05cef380446d5445461e5b017bc
Adityakishore0 <adityakishore0074@gmail.com>
2026-02-11 00:14:13 +0530

    refactor: update expandablecards-demo.tsx

--- src/components/demos/expandablecards-demo.tsx
+++ src/components/demos/expandablecards-demo.tsx
@@ -1,5 +1,5 @@
-"use client";
-import ExpandableCards from "@/components/ui/expandable-cards";
+'use client';
+import ExpandableCards from '@/components/ui/expandable-cards';
 
 export default function ExpandableCardsDemo() {
   const cards = [
@@ -7,9 +7,9 @@ export default function ExpandableCardsDemo() {
       id: 1,
       content: (
         <img
-          src="https://cdn.pixabay.com/photo/2023/06/26/04/38/ai-generated-8088680_1280.jpg"
-          className="w-full h-full object-cover"
-          alt=""
+          src='https://cdn.pixabay.com/photo/2023/06/26/04/38/ai-generated-8088680_1280.jpg'
+          className='w-full h-full object-cover'
+          alt=''
         />
       ),
     },
@@ -17,9 +17,9 @@ export default function ExpandableCardsDemo() {
       id: 2,
       content: (
         <img
-          src="https://cdn.pixabay.com/photo/2024/08/01/18/20/anime-8937917_1280.png"
-          className="w-full h-full object-cover"
-          alt=""
+          src='https://cdn.pixabay.com/photo/2024/08/01/18/20/anime-8937917_1280.png'
+          className='w-full h-full object-cover'
+          alt=''
         />
       ),
     },
@@ -27,9 +27,9 @@ export default function ExpandableCardsDemo() {
       id: 3,
       content: (
         <img
-          src="https://github.com/Adityakishore0.png"
-          className="w-full h-full object-cover"
-          alt=""
+          src='https://github.com/Adityakishore0.png'
+          className='w-full h-full object-cover'
+          alt=''
         />
       ),
     },
@@ -37,9 +37,9 @@ export default function ExpandableCardsDemo() {
       id: 4,
       content: (
         <img
-          src="https://cdn.pixabay.com/photo/2023/02/07/10/50/ai-generated-7773822_1280.jpg"
-          className="w-full h-full object-cover scale-x-[-1]"
-          alt=""
+          src='https://cdn.pixabay.com/photo/2023/02/07/10/50/ai-generated-7773822_1280.jpg'
+          className='w-full h-full object-cover scale-x-[-1]'
+          alt=''
         />
       ),
     },
@@ -47,16 +47,16 @@ export default function ExpandableCardsDemo() {
       id: 5,
       content: (
         <img
-          src="https://cdn.pixabay.com/photo/2023/06/27/03/15/ai-generated-8091289_1280.jpg"
-          className="w-full h-full object-cover"
-          alt=""
+          src='https://cdn.pixabay.com/photo/2023/06/27/03/15/ai-generated-8091289_1280.jpg'
+          className='w-full h-full object-cover'
+          alt=''
         />
       ),
     },
   ];
 
   return (
-    <div className="h-[100px] sm:h-[300px] w-full select-none">
+    <div className='h-25 sm:h-75 w-full select-none'>
       <ExpandableCards cards={cards} defaultExpanded={3} />
     </div>
   );
