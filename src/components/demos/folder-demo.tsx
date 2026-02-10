c750eeb2e1887f1dd919168ed2d0442f70f1d4c7
Adityakishore0 <adityakishore0074@gmail.com>
2026-02-11 00:14:53 +0530

    refactor: update folder-demo.tsx

--- src/components/demos/folder-demo.tsx
+++ src/components/demos/folder-demo.tsx
@@ -1,13 +1,13 @@
-"use client";
+'use client';
 
-import { Folder, FolderContent } from "@/components/ui/folder";
+import { Folder, FolderContent } from '@/components/ui/folder';
 
 export default function FolderDemo() {
   return (
-    <Folder size="xxs">
+    <Folder size='xxs'>
       <FolderContent>
-        <div className="text-center">
-          <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-xl md:text-2xl relative z-20 font-bold tracking-tight">
+        <div className='text-center'>
+          <h2 className='bg-clip-text text-transparent text-center bg-linear-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-xl md:text-2xl relative z-20 font-bold tracking-tight'>
             ScrollX UI
           </h2>
         </div>
