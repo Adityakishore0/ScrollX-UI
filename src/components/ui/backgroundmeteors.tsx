0f6db4e1dd01072716737284c0da8737fa7483f5
Adityakishore0 <adityakishore0074@gmail.com>
2026-02-10 23:49:15 +0530

    refactor: update backgroundmeteors.tsx

--- src/components/ui/backgroundmeteors.tsx
+++ src/components/ui/backgroundmeteors.tsx
@@ -1,6 +1,6 @@
-"use client";
-import React, { useEffect, useState, ReactNode } from "react";
-import { motion } from "framer-motion";
+'use client';
+import React, { useEffect, useState, ReactNode } from 'react';
+import { motion } from 'motion/react';
 
 interface Beam {
   id: number;
@@ -33,7 +33,7 @@ export default function BackgroundMeteors({
       available.splice(
         0,
         available.length,
-        ...available.filter((v) => Math.abs(v - value) > 1)
+        ...available.filter((v) => Math.abs(v - value) > 1),
       );
     }
 
@@ -61,49 +61,49 @@ export default function BackgroundMeteors({
   }, []);
 
   return (
-    <div className="relative flex h-screen w-full items-center justify-center overflow-hidden bg-white dark:bg-black">
+    <div className='relative flex h-screen w-full items-center justify-center overflow-hidden bg-white dark:bg-black'>
       <div
-        className="absolute inset-0"
+        className='absolute inset-0'
         style={{
           backgroundSize: `${gridSize}px ${gridSize}px`,
           backgroundImage:
-            "linear-gradient(to right, #e4e4e7 1px, transparent 1px), linear-gradient(to bottom, #e4e4e7 1px, transparent 1px)",
+            'linear-gradient(to right, #e4e4e7 1px, transparent 1px), linear-gradient(to bottom, #e4e4e7 1px, transparent 1px)',
         }}
       />
       <div
-        className="absolute inset-0 dark:block hidden"
+        className='absolute inset-0 dark:block hidden'
         style={{
           backgroundSize: `${gridSize}px ${gridSize}px`,
           backgroundImage:
-            "linear-gradient(to right, #262626 1px, transparent 1px), linear-gradient(to bottom, #024e6b  1px, transparent 1px)",
+            'linear-gradient(to right, #262626 1px, transparent 1px), linear-gradient(to bottom, #024e6b  1px, transparent 1px)',
         }}
       />
       <div
-        className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white dark:bg-black 
-        [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+        className='pointer-events-none absolute inset-0 flex items-center justify-center bg-white dark:bg-black 
+        mask-[radial-gradient(ellipse_at_center,transparent_20%,black)]'
       />
       {beams.map((b) => (
         <motion.div
           key={b.id}
-          className="absolute top-0"
+          className='absolute top-0'
           style={{ left: b.x, zIndex: 2 }}
           initial={{ y: -150 }}
-          animate={{ y: "100vh" }}
+          animate={{ y: '100vh' }}
           transition={{
             duration: b.duration,
-            ease: "linear",
+            ease: 'linear',
           }}
         >
           <div
-            className="h-14 w-px rounded-full
-              bg-gradient-to-t from-black to-transparent
-              dark:from-indigo-500 dark:via-teal-500 dark:to-transparent"
-            style={{ margin: "0 auto" }}
+            className='h-14 w-px rounded-full
+              bg-linear-to-t from-black to-transparent
+              dark:from-indigo-500 dark:via-teal-500 dark:to-transparent'
+            style={{ margin: '0 auto' }}
           />
         </motion.div>
       ))}
 
-      <div className="absolute inset-0 z-10 flex items-center justify-center">
+      <div className='absolute inset-0 z-10 flex items-center justify-center'>
         {children}
       </div>
     </div>
